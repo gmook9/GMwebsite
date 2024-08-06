ff56f5a870dddd49c2dbde84e9ed3a7e00ace091
gmook9 <gmook9@gmail.com>
2024-08-05 21:24:08 -0700

    added home page placeholder content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "~/styles/globals.css";
+import "../styles/globals.css"; 
 
 import { Inter } from "next/font/google";
 
@@ -19,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-      <html lang="en">
-        <body className={`font-sans ${inter.variable}`}>{children}</body>
-      </html>
+    <html lang="en" className={inter.variable}>
+      <body className="font-sans">{children}</body>
+    </html>
   );
 }
