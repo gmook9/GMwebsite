1980a66b927d27c1a16682a7e1f7ff7d5e9a6bd8
gmook9 <gmook9@gmail.com>
2024-08-10 22:35:38 -0700

    fixed mobile scroll

--- src/app/components/CodeBlock.tsx
+++ src/app/components/CodeBlock.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import Link from 'next/link';
 
 const CodeBlock: React.FC = () => {
-  return (
-    <div className="mockup-code">
-      <pre data-prefix="$"><code>Hello</code></pre>
-      <pre data-prefix=">" className="text-warning"><code>Welcome...</code></pre>
-      <pre data-prefix="$"><code>I am a Software Developer focused on creating impactful digital solutions.</code></pre>
-      <pre data-prefix=">" className="text-success">
-        <code>
-          <Link href="/home" className="hover:underline">
-            Click here to see more!
-          </Link>
-        </code>
-      </pre>
-    </div>
-  );
+    return (
+        <div className="mockup-code" style={{ overflowX: 'hidden' }}>
+            <pre data-prefix="$"><code>Hello</code></pre>
+            <pre data-prefix=">" className="text-warning"><code>Welcome...</code></pre>
+            <pre data-prefix="$"><code>I am a Software Developer focused on creating impactful digital solutions.</code></pre>
+            <pre data-prefix=">" className="text-success">
+                <code>
+                    <Link href="/home" className="hover:underline">
+                        Click here to see more!
+                    </Link>
+                </code>
+            </pre>
+        </div>
+    );
 };
 
 export default CodeBlock;
