09eb10e0e702d8273086366eb1c6b57bbb67a34d
gmook9 <gmook9@gmail.com>
2024-08-13 23:41:25 -0700

    changed 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,13 @@
+'use client'
+import ParticleBackground from '../app/components/ParticleBackground'; // Adjust the import path as needed
+
 export default function NotFound() {
     return (
-      <div style={{ textAlign: 'center', padding: '50px' }}>
-        <h1>404 - Page Not Found</h1>
+      <div style={{ position: 'relative', textAlign: 'center', padding: '50px', height: '100vh', overflow: 'hidden' }}>
+        <ParticleBackground speed={1} />
+        <div style={{ position: 'relative', zIndex: 1 }}>
+          <h1 style={{ color: '#ffffff' }}>404 - Page Not Found</h1>
+        </div>
       </div>
     );
-  }
-  
+}
