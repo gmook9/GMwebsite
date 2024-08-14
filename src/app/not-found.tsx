4f97667f86c5d3fe015f18540ec0cfd81f9e4264
gmook9 <gmook9@gmail.com>
2024-08-13 23:46:19 -0700

    fixed not found (404) page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,13 @@
-'use client'
-import ParticleBackground from '../app/components/ParticleBackground'; // Adjust the import path as needed
+'use client';
+import ParticleBackground from '../app/components/ParticleBackground'; 
+import Alert from '../app/components/Alert';
 
 export default function NotFound() {
     return (
-      <div style={{ position: 'relative', textAlign: 'center', padding: '50px', height: '100vh', overflow: 'hidden' }}>
+      <div style={{ position: 'relative', textAlign: 'center', height: '100vh', overflow: 'hidden', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <ParticleBackground speed={1} />
         <div style={{ position: 'relative', zIndex: 1 }}>
-          <h1 style={{ color: '#ffffff' }}>404 - Page Not Found</h1>
+          <Alert message="404 - Page Not Found" />
         </div>
       </div>
     );
