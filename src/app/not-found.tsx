7498aace0820499dde6425cc223be7d20bc840be
gmook9 <gmook9@gmail.com>
2024-08-15 23:22:47 -0700

    added hover color

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,14 +8,17 @@ export default function NotFound() {
         <ParticleBackground speed={1} />
         <div style={{ position: 'relative', zIndex: 1 }}>
           <Alert message="404 - Page Not Found" />
-          <a href="https://garretmook.com/home" style={{ marginTop: '20px', display: 'inline-block' }}>
+          <a 
+            href="https://garretmook.com/home" 
+            className="mt-5 inline-block"
+          >
             <svg 
               xmlns="http://www.w3.org/2000/svg" 
               width="48" 
               height="48" 
               viewBox="0 0 24 24" 
               fill="white"
-              style={{ cursor: 'pointer' }}
+              className="hover:fill-gray-400 transition-colors duration-200"
             >
               <path d="M21 13v10h-6v-6h-6v6h-6v-10h-3l12-12 12 12h-3zm-1-5.907v-5.093h-3v2.093l3 3z"/>
             </svg>
